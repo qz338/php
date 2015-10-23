e75065e67631ec2e7b305a918ec60984910a62d8
dotcoo <dotcoo@163.com>
2015-10-23 19:52:31 +0800

    添加PDO参数

--- Table/TableAll.php
+++ Table/TableAll.php
@@ -64,7 +64,8 @@ class Table {
 		$options = array(
 				PDO::ATTR_PERSISTENT => true,
 				PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+				// PDO::ATTR_EMULATE_PREPARES => false,
 		);
 		return self::$__pdo = new PDO($dsn, self::$__user, self::$__password, $options);
 	}
